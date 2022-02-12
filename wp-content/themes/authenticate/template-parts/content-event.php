7b8375cb3241523d9577503fa5d8e0aaab3c1d09
Nahid Mahamud Hridoy <nahid_hridoy@outlook.com>
2022-02-12 22:08:54 +0600

    Delete wp-content directory

--- wp-content/themes/authenticate/template-parts/content-event.php
+++
@@ -1,2 +0,0 @@
-<?php include 'single-event/event-info.php' ?>
-<?php get_template_part( 'template-parts/single-event/coming-soon-event' ); ?>
--- wp-content/themes/authenticate/template-parts/pagination.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    the_posts_pagination( array(
-        "screen_reader_text"=>' ',
-        "prev_text"=> __( 'PREV', 'authenticate' ),
-        "next_text"=> __( 'NEXT', 'authenticate' ),
-    ) ); 
-?>
